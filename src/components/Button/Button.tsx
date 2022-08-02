3e8f30683221d28f1a74d735d7354a8cef0ab95e
Lyman Nickolai <frontend@Lymans-MacBook-Pro.local>
2022-08-03 00:03:11 +0300

    add

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import styles from "./Button.module.css" 
 
 const Button = () => {
-    return <button className={styles.button}>Clic Me</button>
+    return <button className={styles.button}>Cli    dfdfdfdc Me</button>
 }
 export default Button
